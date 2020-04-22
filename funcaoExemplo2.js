57fc26f2ff6820fa71a7e9bed7da528a520cfe0b
Paulo Matheus Peruzzo Storrer <paulo.storrer@gmail.com>
2020-04-22 20:51:16 -0300

    fundamento funcao aulas 26,27,28

---
+++ funcaoExemplo2.js
@@ -0,0 +1,19 @@
+//armazenar funcao em uma variavel
+
+const imprimirSoma = function (a, b) {
+    console.log(a+b)
+}
+
+imprimirSoma(2,3)
+
+//armazenando uma funcao arrow em uma variavel
+
+const soma = (a , b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+//retorno implícito
+const subtracao = (a, b) => a-b
+console.log(subtracao(2,3))
