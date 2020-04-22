1fe4f81af61d576126c359c0555d1e94d37150a0
Paulo Matheus Peruzzo Storrer <paulo.storrer@gmail.com>
2020-04-22 15:31:19 -0300

    aula22

---
+++ boolean.js
@@ -0,0 +1,33 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log(!isAtivo)
+
+console.log('Os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+console.log('Os Falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('para finalizar...')
+let nome = ''
+console.log(!!(0 || nome || null || '123' || 'abc'))
+console.log(0 || nome || null || '123' || 'abc')
+nome = 'Lucas'
+console.log(0 || nome || null || '123' || 'abc')
