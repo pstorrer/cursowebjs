cf05611647bec03abcc2bad4323eeb7772c32041
Paulo Matheus Peruzzo Storrer <paulo.storrer@gmail.com>
2020-05-11 16:35:08 -0300

    operadores unarios

---
+++ operadoresUnarios.js
@@ -0,0 +1,11 @@
+let num1 = 1
+let num2 = 2
+
+num1++ //posfix num = num+1
+console.log(num1)
+--num1 //prefix num = num-1
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1===num2)
+
