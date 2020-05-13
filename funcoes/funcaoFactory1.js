ed8b9230444a9b67eab279b336c8794ee12b6eb8
Paulo Matheus Peruzzo Storrer <paulo.storrer@gmail.com>
2020-05-13 14:59:45 -0300

    funcoes factory

---
+++ funcoes/funcaoFactory1.js
@@ -0,0 +1,8 @@
+//factory simples
+function criarPessoa() {
+    return{
+        nome: 'Ana',
+        idade: 30
+    }
+}
+console.log(criarPessoa())
---
+++ funcoes/funcaoFactory2.js
@@ -0,0 +1,10 @@
+function criarProduto(nome, preco){
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Cadeira', 10), criarProduto('mesa', 20))
+
